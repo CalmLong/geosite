e138585d749b056ff59a54a003a40a8c262ffcb2
unknown <37164399+CalmLong@users.noreply.github.com>
2021-03-21 22:13:16 +0800

    增加无效域名缓存

--- ns.go
+++ ns.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"io"
+	"log"
 	"net"
+	"os"
 	"strings"
 	"sync"
+	"time"
 )
 
 func removeSuffix(uri string) (string, bool) {
@@ -24,36 +29,84 @@ func removeSuffix(uri string) (string, bool) {
 }
 
 func handle(originalMap map[string]struct{}, group *sync.WaitGroup, deathChan chan string) {
+	var mutex sync.Mutex
 	for uri := range originalMap {
+		mutex.Lock()
 		go func(uri string) {
 			defer group.Done()
 			if uriStr, ok := removeSuffix(uri); ok {
-				for i := 0; i < 2; i++ {
-					ip, _ := net.LookupIP(uriStr)
-					if len(ip) > 0 {
-						return
-					}
+				ip, _ := net.LookupIP(uriStr)
+				if len(ip) > 0 {
+					return
 				}
 				deathChan <- uri
 			}
 		}(uri)
+		mutex.Unlock()
 	}
 }
 
-func isDeath(originalMap map[string]struct{}) {
-	var group sync.WaitGroup
-	group.Add(len(originalMap))
-	
-	deathChan := make(chan string, 1024)
-	
-	go handle(originalMap, &group, deathChan)
-	
-	go func() {
-		for uri := range deathChan {
-			delete(originalMap, uri)
+func rwCache(valueMap map[string]struct{}, write bool) {
+	if len(valueMap) == 0 {
+		return
+	}
+	const cacheName = "geoSiteDeathCacheList"
+
+	fi, err := os.OpenFile(cacheName, os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if write {
+		for k := range valueMap {
+			_, err := fi.WriteString(k + "\n")
+			if err != nil {
+				log.Fatalln(err)
+			}
 		}
-	}()
-	
+	} else {
+		reader := bufio.NewReader(fi)
+		for {
+			d, _, e := reader.ReadLine()
+			if e == io.EOF {
+				break
+			}
+			delete(valueMap, string(d))
+		}
+	}
+
+	_ = fi.Close()
+}
+
+func isDeath(originalMap map[string]struct{}) {
+	rwCache(originalMap, false)
+
+	nowNum := len(originalMap)
+
+	var group sync.WaitGroup
+	group.Add(nowNum)
+
+	deathChan := make(chan string, nowNum)
+
+	go handle(originalMap, &group, deathChan)
+
 	group.Wait()
-	close(deathChan)
+
+	deathMap := make(map[string]struct{})
+	log.Println("reading death list...")
+
+	timer := time.NewTimer(2 * time.Second)
+	for {
+		select {
+		case <-timer.C:
+			close(deathChan)
+			log.Println("writing death list...")
+			rwCache(deathMap, true)
+			return
+		case uri := <-deathChan:
+			deathMap[uri] = struct{}{}
+			delete(originalMap, uri)
+			timer.Reset(2 * time.Second)
+		}
+	}
 }
