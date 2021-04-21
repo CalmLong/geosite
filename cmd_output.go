49aaec298984082ce8386adbd1a0ee9e0e6d3fa5
CalmLong <37164399+CalmLong@users.noreply.github.com>
2021-04-21 11:24:07 +0800

    代码优化

--- cmd_output.go
+++ cmd_output.go
@@ -2,15 +2,9 @@ package main
 
 func output(action int, originalMap map[string]struct{}) {
 	for k := range originalMap {
-		if v, ok := cover(k, action); ok {
+		if v, ok := toV2Ray(k, action); ok {
 			delete(originalMap, k)
 			originalMap[v] = struct{}{}
 		}
-	}
-}
-
-func outputs(action int, originalMaps ...map[string]struct{}) {
-	for i := 0; i < len(originalMaps); i++ {
-		output(action, originalMaps[i])
 	}
 }
