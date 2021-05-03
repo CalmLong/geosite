7292d498da7aa4d8dc22f10584237e7382315f7e
unknown <37164399+CalmLong@users.noreply.github.com>
2021-05-03 09:12:38 +0800

    输出顺序优化

--- domain.go
+++ domain.go
@@ -13,13 +13,22 @@ import (
 	"time"
 )
 
-func pickWriter(header []string, name string, items ...map[string]struct{}) error {
+func dict2Slice(itemMap map[string]struct{}) []string {
+	item := make([]string, 0)
+	for t := range itemMap {
+		item = append(item, t)
+	}
+	sort.Strings(item)
+	return item
+}
+
+func pickWriter(header []string, name string, items ...[]string) error {
 	buff := bytes.NewBuffer([]byte{})
 	for _, h := range header {
 		buff.WriteString(h)
 	}
 	for _, item := range items {
-		for s := range item {
+		for _, s := range item {
 			buff.WriteString(s)
 		}
 	}
@@ -31,20 +40,20 @@ func autoPick(f string) {
 	switch f {
 	case "clash":
 		const rule = "  - DOMAIN,;;  - DOMAIN-SUFFIX,;"
-		
-		block := load2Format(rule, blockList)
-		cn := load2Format(rule, cnList)
-		proxy := load2Format(rule, proxyList)
-		
+
+		blockFull, blockDomain := load2Format(rule, blockList)
+		cnFull, cnDomain := load2Format(rule, cnList)
+		proxyFull, proxyDomain := load2Format(rule, proxyList)
+
 		header := []string{"# TIME: ", t, "\n", "payload:", "\n"}
-		
-		if err := pickWriter(header, "block.yaml", block); err != nil {
+
+		if err := pickWriter(header, "block.yaml", blockFull, blockDomain); err != nil {
 			log.Fatalln(err)
 		}
-		if err := pickWriter(header, "cn.yaml", cn); err != nil {
+		if err := pickWriter(header, "cn.yaml", cnFull, cnDomain); err != nil {
 			log.Fatalln(err)
 		}
-		if err := pickWriter(header, "proxy.yaml", proxy); err != nil {
+		if err := pickWriter(header, "proxy.yaml", proxyFull, proxyDomain); err != nil {
 			log.Fatalln(err)
 		}
 		log.Println("clash.yaml has been generated successfully.")
@@ -53,28 +62,30 @@ func autoPick(f string) {
 	}
 }
 
-func load2Format(format string, domainList map[string]struct{}) map[string]struct{} {
+func load2Format(format string, domainList map[string]struct{}) ([]string, []string) {
 	f := strings.Split(format, ";")
 	if len(f) != 4 {
 		log.Fatalln("format err: ", format)
 	}
-	
-	buff := make(map[string]struct{})
+
+	full := make(map[string]struct{})
+	domain := make(map[string]struct{})
 	for k := range domainList {
 		if strings.Contains(k, suffixFull) {
 			delete(domainList, k)
 			k = replace(k, suffixFull, f[0], f[1]) + "\n"
-			buff[k] = struct{}{}
+			full[k] = struct{}{}
 			continue
 		}
 		if strings.Contains(k, suffixDomain) {
 			delete(domainList, k)
 			k = replace(k, suffixDomain, f[2], f[3]) + "\n"
-			buff[k] = struct{}{}
+			domain[k] = struct{}{}
 			continue
 		}
 	}
-	return buff
+
+	return dict2Slice(full), dict2Slice(domain)
 }
 
 func loadEntry() map[string]*List {
@@ -91,7 +102,7 @@ func getEntry(name string, value map[string]struct{}) *List {
 	full := make([]string, 0)
 	domain := make([]string, 0)
 	other := make([]string, 0)
-	
+
 	for k := range value {
 		if strings.Contains(k, suffixFull) {
 			full = append(full, k)
@@ -103,18 +114,18 @@ func getEntry(name string, value map[string]struct{}) *List {
 		}
 		other = append(other, k)
 	}
-	
+
 	sort.Strings(full)
 	sort.Strings(domain)
 	sort.Strings(other)
-	
+
 	lines := make([]string, 0)
 	lines = append(append(full, domain...), other...)
-	
+
 	list := &List{
 		Name: name,
 	}
-	
+
 	for _, line := range lines {
 		entry, err := parseEntry(line)
 		if err != nil {
@@ -161,17 +172,17 @@ func init() {
 		Resolve(getBodyFromUrls(block), blockList)
 		ResolveV2Ray(getBodyFromUrls(blockUrlsForV2Ray), blockList)
 	}
-	
+
 	log.Println("init suffix list ...")
 	initSuffix(suffixListRaw)
-	
+
 	log.Println("init allow list ...")
 	Resolve(getBodyFromUrls(allowUrls), allowList)
-	
+
 	log.Println("init cn list ...")
 	Resolve(getBodyFromUrls(directUrls), cnList)
 	ResolveV2Ray(getBodyFromUrls(directUrlsForV2Ray), cnList)
-	
+
 	log.Println("init proxy list ...")
 	ResolveV2Ray(getBodyFromUrls([]string{v2rayNotCn}), proxyList)
 }
